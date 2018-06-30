81ed8affe88c3a63190e06433c844312d6f199a1
Anniekobia <kobiaannunziata94@gmail.com>
2018-06-30 11:53:25 +0300

    Test Register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
 		        'scope' => '',
 		    ],
 		]);
-        return "huyu"
+        return "this";
 		return response(['data'=>json_decode((string) $response->getBody(), true)]);
     }
     public function login(Request $request){

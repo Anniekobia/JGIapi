e4464013c680a3f06112b7cd5bf8e5449c411a7c
louisa <Annie>
2018-07-03 11:32:21 +0300

    API test

--- app/Http/Controllers/API/AppUserController.php
+++ app/Http/Controllers/API/AppUserController.php
@@ -12,34 +12,40 @@ class AppUserController extends Controller
 //	function __construct(){
 //		return $this->middleware('auth:api');
 //	}
-    public function store(Request $request){
-    	$appuser=$request->user()->appuser()->create($request->all());
-    	return new AppUserResource($appuser);
+    public function store(Request $request)
+    {
+        $appuser = $request->user()->appuser()->create($request->all());
+        return new AppUserResource($appuser);
     }
 
-    public function index(Request $request){
-	    return "Annie";
-//    	$AppUsers = AppUser::all();
+    public function index(Request $request)
+    {
+
+        $AppUsers = AppUser::all();
 //    	return "No error";
-    	//return AppUserResource::collection($AppUsers);
+        return AppUserResource::collection($AppUsers);
     }
 
-    public function destroy(Request $request){
-    	
+    public function destroy(Request $request)
+    {
+
     }
 
-    public function update(Request $request,AppUser $appuser){
-    	if ($request->user()->id!==$appuser->user_id) {
-    		return response()->json(['error'=>'Unauthorised action'],401);
-    	}
-    	$appuser=appuser()->create($request->all());
-    	return new AppUserResource($appuser);
+    public function update(Request $request, AppUser $appuser)
+    {
+        if ($request->user()->id !== $appuser->user_id) {
+            return response()->json(['error' => 'Unauthorised action'], 401);
+        }
+        $appuser = appuser()->create($request->all());
+        return new AppUserResource($appuser);
     }
-    public function show($userid){
-    	$userdata= AppUser::find($userid);
-    	if (!$userdata) {
-    		return "User not found";
-    	}
-    	return new AppUserResource($userdata);
+
+    public function show($userid)
+    {
+        $userdata = AppUser::find($userid);
+        if (!$userdata) {
+            return "User not found";
+        }
+        return new AppUserResource($userdata);
     }
 }

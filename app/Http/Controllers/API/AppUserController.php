4aa3879d75e3d831baafe71fad963b17e68217fa
louisa <Annie>
2018-07-04 15:16:36 +0300

    Changes

--- app/Http/Controllers/API/AppUserController.php
+++ app/Http/Controllers/API/AppUserController.php
@@ -6,6 +6,7 @@ use App\AppUser;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AppUserResource;
+use Illuminate\Support\Facades\Hash;
 
 class AppUserController extends Controller
 {

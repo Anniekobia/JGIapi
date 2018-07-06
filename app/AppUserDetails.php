e2c829bbcedb6d9cc93cb2257445d35da74585dc
louisa <Annie>
2018-07-06 04:47:46 +0300

    Working

--- app/AppUserDetails.php
+++ app/AppUserDetails.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class AppUserDetails extends Model
 {
     //
-    protected $fillable=['age','gender','homegym','weight','goal'];
 }
---
+++ database/migrations/2018_07_06_013131_create_app_user_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_user_details', function (Blueprint $table) {
+            $table->Integer('user_id')->unsigned();
+            $table->Foreign('user_id')->references('id')->on('app_users');
+            $table->Integer('age');
+            $table->String('gender');
+            $table->Integer('weight');
+            $table->Integer('weight_goal');
+            $table->String('home_gym_locations');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_user_details');
+    }
+}

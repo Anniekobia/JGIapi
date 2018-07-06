26a6c3e888661b21bfab1ec68fc14fe404298d8d
louisa <Annie>
2018-07-06 05:28:36 +0300

    Update User details

--- app/Http/Controllers/API/AppUserDetailsController.php
+++ app/Http/Controllers/API/AppUserDetailsController.php
@@ -14,7 +14,18 @@ class AppUserDetailsController extends Controller
         $storemethodresponse = array();
         $user = AppUser::where('email', $request->email)->first();
 
-        if ($user){
+        if ($request->age==null||$request->gender==null||$request->weight==null||$request->weight_goal==null||$request->home_gym_location){
+            $userdetails=new AppUserDetails;
+            $userdetails->user_id = $user->id;
+            $userdetails->age = $request->age;
+            $userdetails->gender = $request->gender;
+            $userdetails->weight = $request->weight;
+            $userdetails->weight_goal = $request->weightgoal;
+            $userdetails->home_gym_location = $request->homegymlocation;
+            $userdetails->save();
+            $storemethodresponse['status']=1;
+            $storemethodresponse['message']="Successfully updated";
+        }else if($user){
             $userdetails=new AppUserDetails;
             $userdetails->user_id = $user->id;
             $userdetails->age = $request->age;
@@ -25,9 +36,6 @@ class AppUserDetailsController extends Controller
             $userdetails->save();
             $storemethodresponse['status']=1;
             $storemethodresponse['message']="Succesfully added";
-        }else{
-            $storemethodresponse['status']=2;
-            $storemethodresponse['message']="No such user";
         }
     }
 }

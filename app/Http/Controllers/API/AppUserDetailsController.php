300a2b696810c66dc934de2d7437b3f489878a78
louisa <Annie>
2018-07-06 06:43:15 +0300

    Update User details

--- app/Http/Controllers/API/AppUserDetailsController.php
+++ app/Http/Controllers/API/AppUserDetailsController.php
@@ -14,27 +14,9 @@ class AppUserDetailsController extends Controller
         $storemethodresponse = array();
         $user = AppUser::where('email', $request->email)->first();
 
-        if ($request->age==null||$request->gender==null||$request->weight==null||$request->weight_goal==null||$request->home_gym_location){
-            $userdetails=new AppUserDetails;
-            $age=$request->age;
-            $gender=$request->gender;
-            $weight=$request->weight;
-            $weightgoal=$request->weightgoal;
-            $homegym=$request->homegym;
-
-            $userdetails->user_id = $user->id;
-            $userdetails->age = $age;
-            $userdetails->gender = $gender;
-            $userdetails->weight = $weight;
-            $userdetails->weight_goal = $weightgoal;
-            $userdetails->home_gym_location = $homegym;
-            $userdetails->save();
-            $storemethodresponse['status']=1;
-            $storemethodresponse['age']=$age;
-            $storemethodresponse['gender']=$gender;
-            $storemethodresponse['weight']=$weight;
-            $storemethodresponse['weightgoal']=$weightgoal;
-            $storemethodresponse['homegym']=$homegym;
+        if ($request->age==null||$request->gender==null||$request->weight==null||$request->weightgoal==null||$request->homegym){
+            $storemethodresponse['status']=2;
+            $storemethodresponse['message']="Please fill in all the details";
             return $storemethodresponse;
         }else if($user){
             $userdetails=new AppUserDetails;

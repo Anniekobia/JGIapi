15eeafeb308b7227fb3a2c8e9c9ab9b7255b2a0d
louisa <Annie>
2018-07-06 05:47:10 +0300

    Update User details

--- app/Http/Controllers/API/AppUserDetailsController.php
+++ app/Http/Controllers/API/AppUserDetailsController.php
@@ -16,27 +16,47 @@ class AppUserDetailsController extends Controller
 
         if ($request->age==null||$request->gender==null||$request->weight==null||$request->weight_goal==null||$request->home_gym_location){
             $userdetails=new AppUserDetails;
+            $age=$request->age;
+            $gender=$request->gender;
+            $weight=$request->weight;
+            $weightgoal=$request->weightgoal;
+            $homegym=$request->homegym;
+
             $userdetails->user_id = $user->id;
-            $userdetails->age = $request->age;
-            $userdetails->gender = $request->gender;
-            $userdetails->weight = $request->weight;
-            $userdetails->weight_goal = $request->weightgoal;
-            $userdetails->home_gym_location = $request->homegymlocation;
+            $userdetails->age = $age;
+            $userdetails->gender = $gender;
+            $userdetails->weight = $weight;
+            $userdetails->weight_goal = $weightgoal;
+            $userdetails->home_gym_location = $homegym;
             $userdetails->save();
             $storemethodresponse['status']=1;
-            $storemethodresponse['message']="Successfully updated";
+            $storemethodresponse['age']=$age;
+            $storemethodresponse['gender']=$gender;
+            $storemethodresponse['weight']=$weight;
+            $storemethodresponse['weightgoal']=$weightgoal;
+            $storemethodresponse['homegym']=$homegym;
             return $storemethodresponse;
         }else if($user){
             $userdetails=new AppUserDetails;
+            $age=$request->age;
+            $gender=$request->gender;
+            $weight=$request->weight;
+            $weightgoal=$request->weightgoal;
+            $homegym=$request->homegym;
+
             $userdetails->user_id = $user->id;
-            $userdetails->age = $request->age;
-            $userdetails->gender = $request->gender;
-            $userdetails->weight = $request->weight;
-            $userdetails->weight_goal = $request->weightgoal;
-            $userdetails->home_gym_location = $request->homegymlocation;
+            $userdetails->age = $age;
+            $userdetails->gender = $gender;
+            $userdetails->weight = $weight;
+            $userdetails->weight_goal = $weightgoal;
+            $userdetails->home_gym_location = $homegym;
             $userdetails->save();
             $storemethodresponse['status']=1;
-            $storemethodresponse['message']="Succesfully added";
+            $storemethodresponse['age']=$age;
+            $storemethodresponse['gender']=$gender;
+            $storemethodresponse['weight']=$weight;
+            $storemethodresponse['weightgoal']=$weightgoal;
+            $storemethodresponse['homegym']=$homegym;
             return $storemethodresponse;
         }
     }
